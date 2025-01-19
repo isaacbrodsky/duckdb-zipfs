b49e451ddb22bc457db601962e1fb4673ef8fb32
Isaac Brodsky <isaac@isaacbrodsky.com>
2025-01-19 09:58:20 -0800

    support for duckdb 1.2.0

--- src/include/zip_file_system.hpp
+++ src/include/zip_file_system.hpp
@@ -15,8 +15,7 @@ public:
                 FileOpenFlags flags, unique_ptr<FileHandle> inner_handle_p,
                 const mz_zip_archive_file_stat &file_stat,
                 unique_ptr<data_t[]> data)
-      : // TODO: For main duckdb, pass `flags` here too
-        FileHandle(file_system, path), inner_handle(std::move(inner_handle_p)),
+      : FileHandle(file_system, path, flags), inner_handle(std::move(inner_handle_p)),
         file_stat(file_stat), data(std::move(data)), seek_offset(0) {}
 
   void Close() override;
